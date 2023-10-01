c6fed0269cc4de14b7aa46386479c9b1daa7bb79
Rafael Ventura <73330864+RafaelVCVentura@users.noreply.github.com>
2023-10-01 17:39:50 -0300

    feat: Singleton em GerenciaLivraria

--- classes.py
+++ classes.py
@@ -45,6 +45,13 @@ class Livros:
         return self.__idioma
 
 class GerenciaLivraria:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
         self.conexao = mysql.connector.connect (
             host = credencial_nome,
