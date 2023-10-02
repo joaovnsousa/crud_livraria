b2a42f2ff24d0dabd77ecdab6367dd5c1f798e2d
Rafael Ventura <73330864+RafaelVCVentura@users.noreply.github.com>
2023-10-02 14:25:34 -0300

    fix: verifica_isbn

--- menu.py
+++ menu.py
@@ -1,5 +1,4 @@
 from classes import Livros, GerenciaLivraria
-#import requests
 
 class Menu:
     def __init__(self):
@@ -53,23 +52,22 @@ class Menu:
 
                 preco = float(input('Preço: '))
                 if preco < 0:
-                    raise ValueError("o preço não pode ser um número negativo.")
+                    raise ValueError("O preço não pode ser um número negativo.")
 
                 data_publicacao = input('Data de Publicação (AAAA-MM-DD): ')
                 
                 edicao = int(input('Edição: '))
                 if edicao <= 0:
-                    raise ValueError("o livro deve ter pelo menos uma edição.")
+                    raise ValueError("O livro deve ter pelo menos uma edição.")
                 
 
                 isbn = input('ISBN: ')
                 
                 volume = int(input('Volume: '))
                 if volume < 0:
-                    raise ValueError("o volume não pode ser um número negativo. Digite 0 caso não haja volume.")
+                    raise ValueError("O volume não pode ser um número negativo. Digite 0 caso não haja volume.")
                
                 idioma = input('Idioma: ')
-
 
                 # tratamento de erros:
 
@@ -83,54 +81,52 @@ class Menu:
                     raise ValueError("O livro deve ter pelo menos uma edição.")
                 
                 if self.checa_isbn(isbn) == False:
-                    raise ValueError("O isbn deve ser inserido corretamente.")
+                    raise ValueError("O ISBN deve ser inserido corretamente.")
                 
 
                 novo_livro = Livros(
-                titulo,
-                autor,
-                genero,
-                editora,
-                preco,
-                data_publicacao,
-                edicao,
-                isbn,
-                volume,
-                idioma
+                    titulo,
+                    autor,
+                    genero,
+                    editora,
+                    preco,
+                    data_publicacao,
+                    edicao,
+                    isbn,
+                    volume,
+                    idioma
                 )
             
                 self.instancia_livraria.inserir_livros(novo_livro)
 
-
-            
                 print('Livro inserido com sucesso!')
 
                 return
-            
 
             except ValueError as e:
                 # Captura exceções ValueError quando as entradas são inválidas
                 print(f"Erro: {e}")
                 print("Por favor, forneça entradas válidas.")
 
-
+    @staticmethod
     def verifica_isbn_10(isbn):
         soma = 0
         for i in range(1, len(isbn) + 1):
             soma = soma + int (isbn[i-1])* i
         return soma % 11 == 0
 
+    @staticmethod
     def verifica_isbn_13(isbn):
         soma = (sum(int(caractere) for caractere in isbn[::2]) + sum(int(caractere) * 3 for caractere in isbn[1::2]))
         return soma % 10 == 0
     
-    def checa_isbn(isbn, self):
+    def checa_isbn(self, isbn):
         validacao = False
         isbn = isbn.replace('-', '').replace(' ', '')
         if len(isbn) == 10:
             validacao = self.verifica_isbn_10(isbn)
 
-        if len(isbn) == 13:
+        elif len(isbn) == 13:
             validacao = self.verifica_isbn_13(isbn)
         
         else:
